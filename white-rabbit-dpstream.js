a1377d24170d0bf22ec415baf7bfaa8449be7910
Webwalker <31214542+wbwlkr@users.noreply.github.com>
2017-11-08 12:26:16 +0100

    Modify code after jslint analysis

--- white-rabbit-dpstream.js
+++ white-rabbit-dpstream.js
@@ -1,11 +1,13 @@
-/*
-White Rabbit :
-Redirect to the external url without waiting for the countdown, in https://www.dpstream.net/external_link/*
-*/
+/*White Rabbit :
+(in https://www.dpstream.net/external_link/ *)
+Redirect to the external url without waiting for the countdown.*/
+
+/*jslint browser:true */
+/*global document, XPathResult, window*/
 
 // Search for the hidden url
-var stash = document.querySelectorAll('script:last-child')[0].textContent;
-var stash = document.evaluate("//script[contains(., 'tennsec')]", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+var stash = document.evaluate("//script[contains(., 'tennsec')]", document, null, XPathResult.ANY_TYPE, null)
+                    .iterateNext().textContent;
 var re = /(window.open\(\\\')(.*)(\\\'\,\\\'_blank)/;
 var hidden_link = stash.match(re)[2];
 
