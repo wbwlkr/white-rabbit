03101b8ae6cacc38b801e79a09f7227e0c5393eb
Webwalker <31214542+wbwlkr@users.noreply.github.com>
2017-11-08 20:02:06 +0100

    Add content scripts for openload and streamango

---
+++ wr-openload.js
@@ -0,0 +1,7 @@
+/*White Rabbit :
+(in *://openload.co/embed/ *)
+Delete video overlay element.*/
+
+/*jslint browser:true */
+
+document.getElementById("videooverlay").remove();
---
+++ wr-streamango.js
@@ -0,0 +1,7 @@
+/*White Rabbit :
+(in *://streamango.com/embed/ *)
+Delete video overlay element.*/
+
+/*jslint browser:true */
+
+document.getElementById("videooverlay").remove();
